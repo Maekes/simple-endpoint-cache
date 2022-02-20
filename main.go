09807b7238753e4f48a83260f5ef6073a97e2e0e
Markus Marcinek <markus.marcinek@outlook.de>
2022-02-20 10:18:11 +0100

    enable cores

--- main.go
+++ main.go
@@ -36,6 +36,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	mutex.RLock()
 	defer mutex.RUnlock()
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token")
+	w.Header().Set("Access-Control-Expose-Headers", "Authorization")
 	w.Write(cache)
 }
 
